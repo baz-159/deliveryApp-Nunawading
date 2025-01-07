8f154e021c3d32c67dce71d29a341d038d8ba7fd
baz-159 <baz.polimos@gmail.com>
2025-01-07 13:17:19 +1100

    Updated root address, Title, removed fixed suburbs

--- script.js
+++ script.js
@@ -8,10 +8,7 @@ fetch('Suburb.json')
 
 // Fixed Price Suburbs List
 const fixedPriceSuburbs = {
-    'Craigieburn': 90,
-    'Doreen': 90,
-    'Mernda': 90,
-    'Roxburgh Park': 90
+    
 };
 
 // Rest of your existing script.js code...
@@ -60,7 +57,7 @@ document.getElementById('deliveryForm').addEventListener('submit', function(even
     let customerLocation = document.getElementById('location').value;
 
     // Always calculate and display the route
-    calculateAndDisplayRoute('308-320 Settlement Rd, Thomastown VIC 3074', customerLocation);
+    calculateAndDisplayRoute('396-408 Whitehorse Rd, Nunawading VIC 3131', customerLocation);
 
     let fixedPriceSuburb = Object.keys(fixedPriceSuburbs).find(suburb => customerLocation.includes(suburb));
     if (fixedPriceSuburb) {
@@ -79,7 +76,7 @@ function calculateDistance(customerLocation) {
     var service = new google.maps.DistanceMatrixService();
     service.getDistanceMatrix(
         {
-            origins: ['308-320 Settlement Rd, Thomastown VIC 3074'],
+            origins: ['396-408 Whitehorse Rd, Nunawading VIC 3131'],
             destinations: [customerLocation],
             travelMode: 'DRIVING',
             unitSystem: google.maps.UnitSystem.METRIC,
@@ -197,17 +194,17 @@ function calculateDeliveryPrice(distanceInMeters) {
     } else if (distanceInKm <= 15) {
         calculatedPrice = 90;
     } else if (distanceInKm <= 20) {
-        calculatedPrice = 100;
-    } else if (distanceInKm <= 25) {
         calculatedPrice = 110;
-    } else if (distanceInKm <= 35) {
+    } else if (distanceInKm <= 25) {
         calculatedPrice = 120;
-    } else if (distanceInKm <= 45) {
+    } else if (distanceInKm <= 35) {
         calculatedPrice = 130;
-    } else if (distanceInKm <= 55) {
+    } else if (distanceInKm <= 45) {
         calculatedPrice = 140;
+    } else if (distanceInKm <= 55) {
+        calculatedPrice = 150;
     } else if (distanceInKm <= 65) {
-        calculatedPrice = 160;
+        calculatedPrice = 170;
     } else if (distanceInKm <= 75) {
         calculatedPrice = 190;
     } else if (distanceInKm <= 85) {
